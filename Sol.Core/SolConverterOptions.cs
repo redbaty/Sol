dcb386f075eb381559b9efc5849aff6033385ad2
Marcos Cordeiro <juninhowblu@gmail.com>
2020-07-30 20:40:50 -0300

    Create SolConverterOptions.cs

---
+++ Sol.Core/SolConverterOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace Sol.Core
+{
+    public class SolConverterOptions
+    {
+        public static SolConverterOptions Default = new SolConverterOptions {IgnoreNullOnlyRows = true};
+
+        public bool IgnoreNullOnlyRows { get; set; }
+
+        public bool WriteFormatted { get; set; }
+    }
+}
